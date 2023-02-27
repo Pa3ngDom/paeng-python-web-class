c89ed406d06dc810e6b165bbec418c4edf8488df
Pa3ngDom <126373339+Pa3ngDom@users.noreply.github.com>
2023-02-27 04:38:23 +0000

    new template, add jsonify

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Houston, TX',
+    'salary': '$48,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Houston, TX',
+    
+  },
+  {
+    'id': 3,
+    'title': 'Registered Nurse',
+    'location': 'Pearland, TX',
+    'salary': '$90,000'  
+  }
+]
+
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS)
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
